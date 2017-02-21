b8c6cdbd208149f93c9208475c9c06c32e572e56
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-02-21 12:34:47 +0530

    formatted code

--- Bike-Sharing-Demand/Python/Bike-Sharing-model.py
+++ Bike-Sharing-Demand/Python/Bike-Sharing-model.py
@@ -1,16 +1,14 @@
 import numpy as np
 import pandas as pd
-
 from sklearn.linear_model import LinearRegression
-
 
 def load_fromLocal(data_file):        
     data = pd.read_csv(data_file)    
-    return  data
+    return data
     
 def load_fromUrl(url):   
-    data = pd.read_csv(url,parse_dates=[0])
-    return  data    
+    data = pd.read_csv(url, parse_dates=[0])
+    return data    
 
 def backup_data(train, test):
     # make a copy of the data
@@ -28,30 +26,23 @@ def reformat_datecolumn(data):
     return data
 
 if __name__ =="__main__":
-
 	# Data Source
 	train_url = "https://raw.githubusercontent.com/jesford/bike-sharing/master/train.csv"
 	test_url = "https://raw.githubusercontent.com/jesford/bike-sharing/master/test.csv"
-
-
 	# Load Data
 	train_data =  load_fromUrl(train_url)
 	test_data =  load_fromUrl(test_url)
 	train_orig, test_orig = backup_data(train_data, test_data)
-
 	# Data Preparation
 	new_train_data =  reformat_datecolumn(train_data)        
 	new_test_data =  reformat_datecolumn(test_data)        
-
 	# So we should transform the target columns into log domain as well.
 	for col in ['casual', 'registered', 'count']:
     	new_train_data['log-' + col] = new_train_data[col].apply(lambda x: np.log1p(x))
-
 	# Define features vector
 	features = ['season', 'holiday', 'workingday', 'weather',
 	            'temp', 'atemp', 'humidity', 'windspeed', 'year',
 	             'month', 'weekday', 'hour']
-
 	# Simple Model
 	clf = LinearRegression()
 	clf.fit(new_train_data[features], new_train_data['log-count'])
