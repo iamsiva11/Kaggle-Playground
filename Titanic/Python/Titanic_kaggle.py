21ba760897fd64210e53ebbbd3b50d9d873bdf62
Siva Sundaramoorthy <siva11.sundaramoorthy@gmail.com>
2017-03-02 12:31:30 +0530

    Data prep cont.

--- Titanic/Python/Titanic_kaggle.py
+++ Titanic/Python/Titanic_kaggle.py
@@ -15,6 +15,10 @@ def backup_data(train, test):
     test_orig = test.copy()
     return train_orig, test_orig
 
+
+checkNan = lambda dataset:dataset.isnull().any()
+
+
 if __name__=="__main__":
 	train_url = "https://raw.githubusercontent.com/agconti/kaggle-titanic/master/data/train.csv"
 	test_url ="https://raw.githubusercontent.com/agconti/kaggle-titanic/master/data/test.csv"
@@ -22,3 +26,11 @@ if __name__=="__main__":
 	# Load Data
 	train_data = load_fromUrl (train_url)
 	test_data  = load_fromUrl (test_url)
+
+	# Data prep
+	train_data_ver1 = train_data.drop(["PassengerId","Ticket","Cabin"], axis = 1)
+	train_data_ver1.head(5)
+
+	# Show columns that have Nan values
+	checkNan(train_data_ver1)
+
